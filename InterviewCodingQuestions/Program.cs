8ef8b986078520eee925599a57399cd20f67a609
Vindhya Patamsetti <Vindhya124@gmail.com>
2020-08-28 15:14:32 +1200

    Add project files.

---
+++ InterviewCodingQuestions/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Runtime.Serialization.Json;
+
+namespace InterviewCodingQuestions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var inputArray = new int[] {10, 20, 30, 40, 50};
+            ReverseArray(inputArray);
+        }
+
+        private static void ReverseArray(int[] inputArray)
+        {
+            foreach (var item in inputArray) Console.WriteLine($"Before reversing array {item}");
+            int temp;
+            int i = 0;
+            int j = inputArray.Length - 1;
+            while (i < j)
+            {
+                temp = inputArray[j];
+                inputArray[j] = inputArray[i];
+                inputArray[i] = temp;
+                i++;
+                j--;
+
+            }
+            foreach (var item in inputArray) Console.WriteLine($"After reversing array {item}");
+        }
+    }
+}
